bce706e6ab60e3db99cc733ac841490d773fa58e
Arkady Polhovsky <arkims@inbox.ru>
2020-05-13 23:21:48 +0300

    update webpack to v5

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const PnpWebpackPlugin = require(`pnp-webpack-plugin`);
 
 module.exports = {
-  context: path.join(__dirname, "/src"),
+  context: path.resolve(__dirname, "src"),
   entry: "./index.jsx",
   output: {
-    path: path.join(__dirname, "/dist"),
+    path: path.resolve(__dirname, "dist"),
     filename: "index_bundle.js",
   },
   module: {
@@ -16,7 +15,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: require.resolve("babel-loader"),
+          loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env"],
             plugins: ["@babel/plugin-transform-runtime"],
@@ -27,7 +26,7 @@ module.exports = {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: {
-          loader: require.resolve("babel-loader"),
+          loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env", "@babel/preset-react"],
             plugins: ["@babel/plugin-transform-runtime"],
@@ -41,16 +40,10 @@ module.exports = {
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "./index.html",
     }),
   ],
-  resolve: {
-    plugins: [PnpWebpackPlugin],
-  },
-  resolveLoader: {
-    plugins: [PnpWebpackPlugin.moduleLoader(module)],
-  },
 };
