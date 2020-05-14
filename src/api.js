fe10f1a317e788c22252c0b1f4a6967d2dcd281b
Arkady Polhovsky <arkims@inbox.ru>
2020-05-14 06:02:09 +0300

    add api

---
+++ src/api.js
@@ -0,0 +1,30 @@
+const api = {
+  async getSaves() {
+    const response = await fetch("/saves");
+    const saves = await response.json();
+    return saves;
+  },
+  saveGame(save) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(save),
+    };
+    fetch("/saves", options);
+  },
+  async loadGame(save) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify({ name: save }),
+    };
+    const response = await fetch("/save", options);
+    const game = await response.json();
+    return game;
+  },
+};
+export default api;
--- src/redux/thunks.js
+++ src/redux/thunks.js
@@ -1,9 +1,8 @@
 import { GET_SAVES, TOGGLE_ESCAPE, SAVE_GAME, LOAD_GAME } from "./types";
 import { toggleDisplay } from "./actions";
-
+import api from "../api";
 export const getSaves = () => async (dispatch) => {
-  const response = await fetch("/saves");
-  const saves = await response.json();
+  const saves = await api.getSaves();
   dispatch({ type: GET_SAVES, payload: saves });
 };
 
@@ -15,29 +14,14 @@ export const toggleEscape = (key) => (dispatch) => {
 export const saveGame = (saveName) => (dispatch, getState) => {
   let game = getState().game;
   let save = { saveName, game };
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify(save),
-  };
-  fetch("/saves", options);
+  api.saveGame(save);
   dispatch({ type: SAVE_GAME, payload: save });
   dispatch(getSaves());
   dispatch(toggleEscape(false));
 };
 
 export const loadGame = (save) => async (dispatch) => {
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json;charset=utf-8",
-    },
-    body: JSON.stringify({ name: save }),
-  };
-  const response = await fetch("/save", options);
-  const game = await response.json();
+  const game = await api.loadGame(save);
   dispatch({ type: LOAD_GAME, payload: game });
   dispatch(toggleDisplay(false));
 };
