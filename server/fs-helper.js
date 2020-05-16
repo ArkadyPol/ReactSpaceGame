0d8b97b2697a40459a57331c363396de21676e25
Arkady Polhovsky <arkims@inbox.ru>
2020-05-16 04:13:07 +0300

    refactoring server

---
+++ server/fs-helper.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+const getTime = (pathToFile) => {
+  const stats = fs.statSync(pathToFile);
+  return stats.mtime;
+};
+
+exports.getSaves = () => {
+  let saves = fs.readdirSync("saves", "utf-8");
+  saves = saves
+    .sort((a, b) => getTime(`saves/${b}`) - getTime(`saves/${a}`))
+    .map((file) => file.split(".")[0]);
+  return saves;
+};
+exports.loadSave = (saveName) => {
+  const save = JSON.parse(
+    fs.readFileSync(`saves/${saveName.name}.json`, "utf-8")
+  );
+  return save;
+};
+exports.createSave = (save) => {
+  const name = save.saveName;
+  fs.writeFileSync(`saves/${name}.json`, JSON.stringify(save.game, null, 4));
+};
