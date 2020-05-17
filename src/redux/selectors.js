2d8bdce47e049856605e38a7e1463bcf47e91c93
Arkady Polhovsky <arkims@inbox.ru>
2020-05-17 04:08:01 +0300

    add reselect

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,8 +1,21 @@
-const getGame = ({ game }) => ({
-  ...game.game,
-  stars: game.stars,
-  passedPath: game.passedPath,
-  ...game.move,
-});
+import { createSelector } from "reselect";
+
+const getRestGame = (state) => state.game.game;
+const getStars = (state) => state.game.stars;
+const getPassedPath = (state) => state.game.passedPath;
+const getMove = (state) => state.game.move;
+
+const getGame = createSelector(
+  getRestGame,
+  getStars,
+  getPassedPath,
+  getMove,
+  (game, stars, passedPath, move) => ({
+    ...game,
+    stars,
+    passedPath,
+    ...move,
+  })
+);
 
 export default getGame;
