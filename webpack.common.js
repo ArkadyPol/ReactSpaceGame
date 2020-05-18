7e72049dfc02ab3a08a1c2ee1f681fbe622f3d69
Arkady Polhovsky <arkims@inbox.ru>
2020-05-18 08:51:06 +0300

    add ts-loader

--- webpack.common.js
+++ webpack.common.js
@@ -2,35 +2,26 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const babelOptions = (presets) => {
-  const opts = {
-    presets: ["@babel/preset-env"],
-    plugins: ["@babel/plugin-transform-runtime"],
-  };
-  if (presets.length) {
-    opts.presets.push(...presets);
-  }
-  return opts;
-};
-
-const babelPresets = {
-  js: [],
-  ts: ["@babel/preset-typescript"],
-  jsx: ["@babel/preset-react"],
-  get tsx() {
-    return [this.jsx[0], this.ts[0]];
-  },
-};
-
 const babelRule = (ext) => {
-  return {
+  const rule = {
     test: new RegExp(`\.${ext}$`),
-    include: path.resolve(__dirname, 'src'),
-    use: {
-      loader: "babel-loader",
-      options: babelOptions(babelPresets[ext]),
-    },
+    include: path.resolve(__dirname, "src"),
+    use: [
+      {
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"],
+          plugins: ["@babel/plugin-transform-runtime"],
+        },
+      },
+    ],
   };
+  if (ext[2] === "x") rule.use[0].options.presets.push("@babel/preset-react");
+  if (ext[0] === "t")
+    rule.use.push({
+      loader: "ts-loader",
+    });
+  return rule;
 };
 
 module.exports = {
