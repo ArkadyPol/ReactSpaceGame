07a1e5ce15a2efe22bd14c11d921730d8f084ec8
Arkady Polhovsky <arkims@inbox.ru>
2020-05-29 14:54:59 +0300

    add chances of boxes color

--- src/redux/reducers/game-reducer/boxes-reducer.ts
+++ src/redux/reducers/game-reducer/boxes-reducer.ts
@@ -1,8 +1,15 @@
 import { LOAD_GAME, UPDATE_GAME, RESET, DROP_BOX } from '../../actions-types';
 import { GameReducerAction, DropBoxAction } from '../../actions';
 import { Box } from '../../../types';
+import randomInteger from '../../../logic';
 
 const initialState = [] as readonly Box[];
+
+const colors = {
+  iron: 'red',
+  gold: 'gold',
+  platinum: '#b1e7fc',
+};
 
 const boxesReducer = (
   state = initialState,
@@ -21,8 +28,19 @@ const boxesReducer = (
         .filter((params) => params.y < 800);
     }
     case DROP_BOX: {
-      const { x, y } = action.payload;
-      const box: Box = { x, y, color: 'red' };
+      const { x, y, size } = action.payload;
+      const chance = ((size - 10) / 90) * 15 + 5;
+      const c = (100 - chance) / chance; // a*x^2 + b*x - c = 0; a = 1; b = 1;
+      const koef = (Math.sqrt(1 + 4 * c) - 1) / 2; // positive solution of a quadratic equation
+      const chances = [chance, chance * koef + chance].map((num) =>
+        Math.round(num)
+      );
+      let color;
+      const percent = randomInteger(0, 99);
+      if (percent < chances[0]) color = colors.platinum;
+      else if (percent < chances[1]) color = colors.gold;
+      else color = colors.iron;
+      const box: Box = { x, y, color };
       return [...state, box];
     }
     case RESET:
